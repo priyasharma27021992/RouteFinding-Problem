8c49e3098f291d41858bd7596a5754c3c0db5339
Priya <priyasharma2702@gmail.com>
2018-09-26 18:41:37 +0530

    Update constants.js

--- src/common/config/constants.js
+++ src/common/config/constants.js
@@ -1,3 +1,3 @@
-export const googleAPIKey = 'AIzaSyAs2KefRNh0_ZQqMbLXeFwFp7myXaB_h7U'; //`AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc`;
+export const googleAPIKey = 'AIzaSyAs2KefRNh0_ZQqMbLXeFwFp7myXaB_h'; //`AIzaSyBG0SybP0EKWH3Jvwki7IR5AMyO_cUeeQc`;
 
 export const baseUrl = 'http://localhost:8080';
